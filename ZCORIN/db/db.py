58c47a8ab7f17f78d2a9d7736639b1364a544b37
Fadeeaal <rakha.fadil@ui.ac.id>
2026-01-15 12:54:37 +0700

    Update folder

--- ZCORIN/db/db.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from sqlalchemy import create_engine
-
-def get_engine():
-    p = st.secrets["postgres"]
-    url = f"postgresql+psycopg2://{p['user']}:{p['password']}@{p['host']}:{p['port']}/{p['database']}"
-    return create_engine(url, pool_pre_ping=True)
